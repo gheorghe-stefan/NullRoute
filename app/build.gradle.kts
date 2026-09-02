c0a2029f0818b1776a4381d2c5588de1b4f9a0ac
ghst <stefangh@gmail.com>
2026-09-02 21:09:09 +0200

    Align version to v1.0.0 for initial Play Store release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = currentBuildNumber
-        versionName = "1.6.0"
+        versionName = "1.0.0"
 
         vectorDrawables {
             useSupportLibrary = true
